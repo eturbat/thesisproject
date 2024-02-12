2ae39403180430856c33d5b343f8f457a7ce6c86
Turbat Enkhtur <61502798+eturbat@users.noreply.github.com>
2024-02-12 18:42:18 -0500

    index UI added

--- index.php
+++ index.php
@@ -5,8 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <title>Welcome to the Oral Defense Scheduling System</title>
     <style>
+        body {
+            font-family: 'Inter', sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+        }
         .logo {
             display: block;
             margin: auto;
